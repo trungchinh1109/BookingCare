1e4916fc74e53b4a98c87fc480c7fd1fe3e6a262
trungchinh1109 <hoquoctrungchinh@gmail.com>
2022-10-06 14:11:35 +0700

    home page ver 1.1

--- src/store/reducers/appReducer.js
+++ src/store/reducers/appReducer.js
@@ -31,6 +31,14 @@ const appReducer = (state = initialState, action) => {
                     ...action.contentOfConfirmModal
                 }
             }
+
+            case actionTypes.CHANGE_LANGUAGE: 
+            console.log('trungchinh check redux :' , action )
+            return {
+                ...state,
+                language: action.language,
+                
+            }
         default:
             return state;
     }
