ffb7128a3534f56b4a55b36377195d9375299b60
trungchinh1109 <hoquoctrungchinh@gmail.com>
2022-10-28 12:46:08 +0700

    admin page ver 1.4

--- src/containers/Header/menuApp.js
+++ src/containers/Header/menuApp.js
@@ -3,32 +3,32 @@ export const adminMenu = [
         name: 'menu.admin.manage-user',
         menus: [
             {
-                name: 'menu.admin.crud',link:'/system/user-manage'
+                name: 'menu.admin.crud', link: '/system/user-manage'
             },
             {
-                name: 'menu.admin.crud-redux',link:'/system/user-redux'
+                name: 'menu.admin.crud-redux', link: '/system/user-redux'
             },
 
             {
-                name: 'menu.admin.manage-doctor',link:'/system/manage-doctor'
+                name: 'menu.admin.manage-doctor', link: '/system/manage-doctor'
                 // subMenus: [
                 //     { name: 'menu.system.system-administrator.user-manage', link: '/system/user-manage' },
                 //     { name: 'menu.system.system-administrator.user-redux', link: '/system/user-redux' },
                 // ]
             },
-            {
-                name: 'menu.admin.manage-admin',link:'/system/user-admin'
+            // {
+            //     name: 'menu.admin.manage-admin',link:'/system/user-admin'
+            // },
+            { //quản lý lịch khám bệnh bác sĩ
+                name: 'menu.doctor.manage-schedule', link: '/doctor/manage-schedule'
             },
-           
-
-
         ]
     },
     { //quản lý phòng khám
         name: 'menu.admin.clinic',
         menus: [
             {
-                name: 'menu.admin.manage-clinic',link:'/system/manage-clinic'
+                name: 'menu.admin.manage-clinic', link: '/system/manage-clinic'
             },
         ]
     },
@@ -36,7 +36,7 @@ export const adminMenu = [
         name: 'menu.admin.specialty',
         menus: [
             {
-                name: 'menu.admin.manage-specialty',link:'/system/manage-specialty'
+                name: 'menu.admin.manage-specialty', link: '/system/manage-specialty'
             },
         ]
     },
@@ -44,8 +44,18 @@ export const adminMenu = [
         name: 'menu.admin.handbook',
         menus: [
             {
-                name: 'menu.admin.manage-handbook',link:'/system/manage-handbook'
+                name: 'menu.admin.manage-handbook', link: '/system/manage-handbook'
             },
         ]
     },
+];
+export const doctorMenu = [
+    {
+        name: 'menu.admin.manage-user',
+        menus: [
+            { //quản lý lịch khám bệnh bác sĩ 
+                name: 'menu.doctor.manage-schedule', link: '/doctor/manage-schedule'
+            },
+        ]
+    }
 ];
--- src/containers/System/Admin/TableManageUser.js
+++ src/containers/System/Admin/TableManageUser.js
@@ -3,9 +3,6 @@ import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import './TableManageUser.scss';
 import * as actions from "../../../store/actions";
-
-
-
 import MarkdownIt from 'markdown-it';
 import MdEditor from 'react-markdown-editor-lite';
 // import style manually
